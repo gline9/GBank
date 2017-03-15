afc959a0b3686b49c1c5c009905ed05aba320892
Gavin Line <gline9@comcast.net>
2017-03-14 23:06:41 -0500

    Adds some basic file handling to the installer

--- linuxInstall.sh
+++ linuxInstall.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-mkdir Accounts
-touch Accounts/userLogins.csv
+if [ $# != 1 ] then
+	echo "Usage ./linuxInstall.sh <Install location>"
+	exit
+fi
+
+set location=$1
+mkdir "$1"/Accounts
+touch "$1"/Accounts/userLogins.csv
+echo "username, salt, password" > "$1"/Accounts/userLogins.csv
