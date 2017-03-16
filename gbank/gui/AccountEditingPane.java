d888ec439a70ed886af8445357513dd62c3b5e9b
Gavin Line <gline9@comcast.net>
2017-03-15 19:54:42 -0500

    Adds the beginning of a new gui for account creation

--- gbank/gui/AccountEditingPane.java
+++ gbank/gui/AccountEditingPane.java
@@ -1,7 +1,6 @@
 package gbank.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.util.HashMap;
@@ -41,20 +40,22 @@ public class AccountEditingPane extends JPanel {
 	}
 
 	private void addAccount(ActionEvent e) {
-		Account a = new Account(1000000, 1, 100);
-
-		// add the new account to the user
-		int id = user.addAccount(a);
-
-		// create a new account pane
-		AccountPane pane = new AccountPane(a, id);
-		pane.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-		// add the account pane to the main window
-		((AccountGui) SwingUtilities.getRoot(this)).addAccount(pane, accounts.size());
-
-		// add the account to the list of accounts
-		accounts.put(id, new Pair<>(pane, a));
+		new CreateAccountGui((JFrame) SwingUtilities.getRoot(this));
+		// Account a = new Account(1000000, 1, 100);
+		//
+		// // add the new account to the user
+		// int id = user.addAccount(a);
+		//
+		// // create a new account pane
+		// AccountPane pane = new AccountPane(a, id);
+		// pane.setAlignmentX(Component.LEFT_ALIGNMENT);
+		//
+		// // add the account pane to the main window
+		// ((AccountGui) SwingUtilities.getRoot(this)).addAccount(pane,
+		// accounts.size());
+		//
+		// // add the account to the list of accounts
+		// accounts.put(id, new Pair<>(pane, a));
 	}
 
 }
---
+++ gbank/gui/CreateAccountGui.java
@@ -0,0 +1,12 @@
+package gbank.gui;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class CreateAccountGui extends JDialog {
+	public CreateAccountGui(JFrame parent) {
+		super(parent, true);
+		setVisible(true);
+		setModal(true);
+	}
+}
