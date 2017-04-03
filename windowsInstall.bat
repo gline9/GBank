9ed21ec74ead7d10660c982ea7a14a3a445170a8
Gavin Line <gline9@comcast.net>
2017-04-02 23:54:39 -0500

    Adds the copy for the main program and adds proper stream redirects to the windows installer bat file

--- windowsInstall.bat
+++ windowsInstall.bat
@@ -1,6 +1,6 @@
 @echo off
-if "%1"=="" goto :usage
-if "%2" NEQ "" goto :usage
+if "%~1"=="" goto :usage
+if "%~2" NEQ "" goto :usage
 
 set location=%1
 
@@ -10,7 +10,10 @@ echo.>%location%\Accounts\userLogins.csv
 echo username^, salt^, password 1>%location%\Accounts\userLogins.csv
 
 REM copy over resources to the correct location
-robocopy resources %location%\resources /s /e 1>null
+robocopy resources %location%\resources /s /e 1>NUL
+
+REM copy over program to the correct location
+copy GBank.jar %location%\GBank.jar 1>NUL
 
 echo Installation complete!
 
